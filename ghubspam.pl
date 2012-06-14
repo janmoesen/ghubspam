e4bec0ed62f6ae272f6a1eba7d4773a9c94ef514
Jan Moesen <github.com-janunix@moesen.nu>
2012-06-14 22:14:19 +0200

    Initial commit
    Based on twatspam.pl

---
+++ ghubspam.pl
@@ -0,0 +1,60 @@
+# == WHAT
+# Show the latest commit for a GitHub repository.
+#
+# == WHO
+# Jan Moesen, 2012
+#
+# == INSTALL
+# Save it in ~/.irssi/scripts/ and do /script load ghubspam.pl
+# OR
+# Save it in ~/.irssi/scripts/autorun and (re)start Irssi.
+
+use strict;
+use Irssi;
+use JSON::XS;
+use LWP::Simple;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.1';
+%IRSSI = (
+	authors     => 'Jan Moesen',
+	name        => 'Ghubspam',
+	description => 'Show the latest commit for a GitHub repository.',
+	license     => 'GPL',
+	url         => 'http://jan.moesen.nu/',
+);
+
+sub ghubspam_process_message {
+	my ($server, $msg, $target) = @_;
+
+	return unless $target =~ /^#(wijs|catena|lolwut)/;
+	return unless $msg =~ m/https:\/\/github\.com\/([^\/]+)\/([^\/]+)/;
+
+	my $user = $1;
+	my $repo = $2;
+
+	my $json_url = "https://api.github.com/repos/$user/$repo";
+	my $json = get($json_url);
+	return unless $json;
+	my $summary = decode_json($json);
+	my $message = "Repository: \"$summary->{name}: $summary->{description}\"";
+	$server->command("msg $target $message");
+
+	my $json_url = "https://api.github.com/repos/$user/$repo/commits/HEAD";
+	my $json = get($json_url);
+	return unless $json;
+	my $commit = decode_json($json);
+	my $message = "Latest commit: \"$commit->{commit}->{message}\" by $commit->{commit}->{author}->{name}";
+	$server->command("msg $target $message");
+}
+
+Irssi::signal_add_last('message public', sub {
+	my ($server, $msg, $nick, $mask, $target) = @_;
+	Irssi::signal_continue($server, $msg, $nick, $mask, $target);
+	ghubspam_process_message($server, $msg, $target);
+});
+Irssi::signal_add_last('message own_public', sub {
+	my ($server, $msg, $target) = @_;
+	Irssi::signal_continue($server, $msg, $target);
+	ghubspam_process_message($server, $msg, $target);
+});
